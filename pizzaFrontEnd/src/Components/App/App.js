e5869150bb55ef259ee05eb1100b9fb8d4d95c8f
Mark Anderson <manderson@diesellaptops.com>
2023-08-30 18:42:19 -0400

    created footer

--- pizzaFrontEnd/src/Components/App/App.js
+++ pizzaFrontEnd/src/Components/App/App.js
@@ -1,11 +1,12 @@
 import styles from "./App.module.css";
 import Header from "../header/Header";
-import ItemCard from "../item-card/ItemCard";
+import Footer from "../footer/Footer";
 
 function App() {
   return (
     <div className={styles.App}>
       <Header />
+      <Footer />
     </div>
   );
 }
---
+++ pizzaFrontEnd/src/Components/footer/Footer.js
@@ -0,0 +1,14 @@
+import React from "react";
+import styles from "./footer.module.css";
+
+function Footer() {
+  return (
+    <div>
+      <div className={styles.footer}>
+        <h4>Created by: GroupOne</h4>
+      </div>
+    </div>
+  );
+}
+
+export default Footer;
