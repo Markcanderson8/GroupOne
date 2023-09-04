126855e7afc5dcc5f8fb384585990dda36e32c76
Mark Anderson <manderson@diesellaptops.com>
2023-09-04 15:21:01 -0400

    implemented single product page

--- pizzaFrontEnd/src/Components/item-card/ItemCard.js
+++ pizzaFrontEnd/src/Components/item-card/ItemCard.js
@@ -1,16 +1,20 @@
 import styles from "./ItemCard.module.css";
 
-const ItemCard = (props) => {
+const ItemCard = ({ product }) => {
   return (
     <div>
       <div className={styles.item}>
         <div className={styles.card}>
-          <img className={styles.img} src={props.itemImg} alt="cheese pizza" />
+          <img
+            className={styles.img}
+            src={product.itemImg}
+            alt="cheese pizza"
+          />
           <div className="card-body">
-            <h2 className={styles.heading}>{props.itemName}</h2>
-            <p>{props.itemName} Pizza</p>
-            <p>Size: {props.itemSize}</p>
-            <p>Price: ${props.itemPrice}</p>
+            <h2 className={styles.heading}>{product.itemName}</h2>
+            <p>{product.itemName} Pizza</p>
+            <p>Size: {product.itemSize}</p>
+            <p>Price: ${product.itemPrice}</p>
           </div>
         </div>
       </div>
