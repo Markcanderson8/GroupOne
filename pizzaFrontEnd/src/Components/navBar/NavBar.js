effb5a681e1d912a1c898e1f656900c4ab44b461
Mark Anderson <manderson@diesellaptops.com>
2023-09-04 13:43:07 -0400

    adjusted the nav links for background and hover

--- pizzaFrontEnd/src/Components/navBar/NavBar.js
+++ pizzaFrontEnd/src/Components/navBar/NavBar.js
@@ -6,13 +6,22 @@ import { Link } from "react-router-dom";
 function NavBar() {
   return (
     <div className={styles.navBar}>
-      <Link className={styles.logoWidth} to="/">
-        <img src={logo} alt="pizza logo" className={styles.logo} />
+      <div className={styles.alignLogoCenter}>
+        <Link className={styles.logoLink} to="/">
+          <img src={logo} alt="pizza logo" className={styles.logo} />
+        </Link>
+      </div>
+
+      <Link className={styles.navLinks} to="/products">
+        Products
       </Link>
-      <Link to="/products">Products</Link>
       <h1>The Pizza Shack</h1>
-      <Link href="#">Register</Link>
-      <Link href="#">Login</Link>
+      <Link className={styles.navLinks} href="#">
+        Register
+      </Link>
+      <Link className={styles.navLinks} href="#">
+        Login
+      </Link>
     </div>
   );
 }
